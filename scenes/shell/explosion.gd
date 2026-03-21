d9b51db738262b6a2ba09cfde92cae1e5f621c06
_lordBucket <antoniuk.orest@gmail.com>
2026-03-21 23:16:25 +0100

    Slight randomisation towards explosions

--- scenes/shell/explosion.gd
+++ scenes/shell/explosion.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite2D
 
 func _ready() -> void:
+	rotation = deg_to_rad(randf_range(1, 360));
+	scale = scale * randf_range(0.9, 1.1);
 	play("default");
 
 func _on_animation_finished() -> void:
