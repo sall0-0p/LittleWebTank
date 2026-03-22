27ac129c55605d00ca40a894c861077e7e6960b8
_lordBucket <antoniuk.orest@gmail.com>
2026-03-22 01:25:10 +0100

    Removed unused assets and code.

--- src/init.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Map.path_to_folder = "res://assets/map/ASTM";
-	print("Up and Running!");
